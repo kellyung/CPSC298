9ba8a653b5e20d310195937254873f0725481af2
kellyung <121912506+kellyung@users.noreply.github.com>
2023-01-08 15:41:19 -0800

    Add files via upload

---
+++ Project1_HelloCpp/hello.cpp
@@ -0,0 +1,10 @@
+/// @file <hello.cpp>
+/// @brief <Prints out “Hello C++”>
+/// @author <kelung@chapman.edu>
+
+#include <iostream>
+int main()
+{
+    std::cout << "Hello C++!" << std::endl;
+    return 0;
+}
